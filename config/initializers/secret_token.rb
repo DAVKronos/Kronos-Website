7771328215fb00c7369ff9fcdcb2b4c0a0f9f715
Wouter Timmermans <protowouter@gmail.com>
2011-05-21 11:59:37 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KronosWebsite::Application.config.secret_token = '007eaf44e927c97278a3fab2ff150d01cc6fafe05d9029ca1cc082c2627f63ac0ddef9e48017b6290f2f726c2d64d8be01c43ee330e7e007522819b51f9ed0ff'
