0a8e834bb354793d5b6e121b44b2b1864779105e
Wouter Timmermans <protowouter@gmail.com>
2011-05-22 14:48:02 +0200

    Added user show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
-end
+  end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find_by_id(params[:id])
+  end
 end
