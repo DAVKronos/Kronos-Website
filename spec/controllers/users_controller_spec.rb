8ddea0c674d0a7310a38387d0676ea856ef1bc9b
Wouter Timmermans <protowouter@gmail.com>
2011-05-23 15:56:32 +0200

    Added users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,5 +9,53 @@ describe UsersController do
       response.should be_success
     end
   end
+  
+  describe "POST 'create'" do
 
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :name => "", :email => "", :address => "",
+                  :initials => "", :birthdate => "",
+                  :city => "", :postalcode => "",
+                  :licensenumber => "", :sex => ""}
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "Henk de Vries", :email => "h.devries@gmail.com", 
+                  :address => "Straatzondernaam 3",
+                  :initials => "H.D.V.",
+                  :birthdate => "16-06-1991",
+                  :city => "Jeruzalem",
+                  :postalcode => "0000 BC",
+                  :licensenumber => "000000",
+                  :sex => "Man" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end    
+    end
+  end
 end
