c347c4a76158c1fdd8eb4e14b87865732f2e169f
Wouter Timmermans <protowouter@gmail.com>
2011-08-27 16:32:38 +0200

    edit functionality implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+  
+  def update
+    @user = User.find_by_id(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
