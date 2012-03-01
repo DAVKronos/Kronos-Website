36a5cf579ec7bf4e2ee8440bfb15daf61f015fda
Wouter Timmermans <protowouter@gmail.com>
2012-03-01 01:53:21 +0100

    Mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,9 @@ KronosWebsite::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.delivery_method = :sendmail
+    config.action_mailer.perform_deliveries = true
 
   # Enable threaded mode
   # config.threadsafe!
