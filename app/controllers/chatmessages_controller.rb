907b4d224bc04b9172de8a291929413625bbf2df
Wouter Timmermans <protowouter@gmail.com>
2012-03-05 16:15:32 +0100

    friendlier form on recaptcha error

--- app/controllers/chatmessages_controller.rb
+++ app/controllers/chatmessages_controller.rb
@@ -56,7 +56,7 @@ class ChatmessagesController < ApplicationController
         format.html { redirect_to chatmessages_path, notice: 'Chatmessage was successfully created.' }
         format.json { render json: @chatmessage, status: :created, location: @chatmessage }
       else
-        format.html { redirect_to chatmessages_path }
+        format.html { render action: "new" }
         format.json { render json: @chatmessage.errors, status: :unprocessable_entity }
       end
     end
