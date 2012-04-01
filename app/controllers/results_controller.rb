bffb4e36721bea4a35fbde850f6b055a18fcdbe7
Leon Schenk <leon_schenk@live.nl>
2012-04-01 15:46:55 +0200

    Revert "Revert "deleted stupide files""
    This reverts commit 0f1c8da89d033ffcd2837354c06d7b91c6886e77.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,8 +3,15 @@ class ResultsController < ApplicationController
   # GET /results
   # GET /results.json
   def index
-    @results = Result.paginate(:page => params[:page], :order => 'updated_at DESC', :per_page => 10)
+    #@results = Result.all
+    
+    @slicedresults = Array.new
 
+    @slicedresults[0] = ["Laatste", Result.where(:created_at => (DateTime.now() - 1.year)..(DateTime.now()))]
+    @slicedresults[1] = ["2012", Result.where(:created_at => (DateTime.now() - 1.year)..(DateTime.now()))]
+    @slicedresults[2] = ["2011", Result.where(:created_at => (DateTime.now() - 1.year)..(DateTime.now()))]
+    @slicedresults[3] = ["2010", Result.where(:created_at => (DateTime.now() - 1.year)..(DateTime.now()))]
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @results }
