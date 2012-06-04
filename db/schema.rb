120dd2927f7402244134752cb3ac6fdee724e659
Wouter Timmermans <protowouter@gmail.com>
2012-06-04 13:18:11 +0200

    oops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120604110319) do
+ActiveRecord::Schema.define(:version => 20120604111556) do
 
   create_table "agendaitems", :force => true do |t|
     t.string   "name"
@@ -245,9 +245,9 @@ ActiveRecord::Schema.define(:version => 20120604110319) do
   create_table "user_types", :force => true do |t|
     t.string   "name"
     t.boolean  "donor"
-    t.boolean  "recreational"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.boolean  "competition"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
