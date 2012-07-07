90184a4e5f869ac85ba11e0cb1afbe07da855cd9
Wouter Timmermans <protowouter@gmail.com>
2012-07-07 13:25:02 +0200

    performance enhancements in results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -6,17 +6,18 @@ class ResultsController < ApplicationController
     #@results = Result.all
     
     @slicedresults = Array.new
+    @matches = Agendaitem.joins(:agendaitemtype).where(:agendaitemtypes => {:is_match => true}).order('date DESC')
 
-    @slicedresults[0] = ["Laatste", Agendaitem.order('date DESC').where("date <= ?", DateTime.now).limit(20)]
+    @slicedresults[0] = ["Laatste", @matches.where("date <= ?", DateTime.now).limit(20)]
     currentyear = DateTime.now.year
     
-    tussenresult = [Date.new(currentyear).year.to_s, Agendaitem.where(:date => (Date.new(currentyear)..DateTime.now()))]
+    tussenresult = [Date.new(currentyear).year.to_s, @matches.where(:date => (Date.new(currentyear)..DateTime.now()))]
     if !tussenresult.last.empty?
       @slicedresults[1] = tussenresult
     end
     
     (2..6).each do |i|
-      tussenresult = [Date.new(currentyear - i + 1).year.to_s, Agendaitem.where(:date => (Date.new(currentyear - i + 1)..Date.new(currentyear - i + 2)))]
+      tussenresult = [Date.new(currentyear - i + 1).year.to_s, @matches.where(:date => (Date.new(currentyear - i + 1)..Date.new(currentyear - i + 2)))]
       if !tussenresult.last.empty?
         @slicedresults[i] = tussenresult
       end
