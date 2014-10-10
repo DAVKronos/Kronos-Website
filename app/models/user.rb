f16fbeae1e262b1ef9df0b8027956b8d20f7d04a
Wouter Timmermans <wouter@woutertimmermans.nl>
2014-10-10 16:25:18 +0200

    refactoring user

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,25 +36,24 @@
 #
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-    
-  before_save :purge_member_from_group, :update_group_membership, :update_commission_email
+
+  before_save :purge_member_from_group, :update_group_membership,
+              :update_commission_email
   after_destroy :remove_member_from_group
-  
+
   attr_accessible :name, :initials, :birthdate,
                   :sex, :licensenumber, :user_type_id,
                   :xtracard, :bank_account_number, :iban, :as => :bestuur
-  
-  
-  attr_accessible :email, :address, :postalcode, 
-                  :city, :phonenumber, :password, :password_confirmation, 
-                  :papieren_kronometer, :avatar, :avatar_file_name, :remember_me, :as => [:default, :bestuur]
-                  
-  
-  
+
+
+  attr_accessible :email, :address, :postalcode,
+                  :city, :phonenumber, :password, :password_confirmation,
+                  :papieren_kronometer, :avatar,
+                  :avatar_file_name, :remember_me, :as => [:default, :bestuur]
+
+
   has_many :commission_memberships, :dependent => :destroy
   has_many :commissions, :through => :commission_memberships
   has_many :chatmessages
@@ -64,9 +63,10 @@ class User < ActiveRecord::Base
   has_many :tags
   has_many :agendaitems
   belongs_to :user_type
-  has_attached_file :avatar, :styles => { :medium => "300x300", :pass => "260x180#", :thumb => "50x50#" }, :path => ":rails_root/public/system/:attachment/:hash.:extension",
-  :url => "/system/:attachment/:hash.:extension", :hash_secret => "longSecretString"
-  
+  has_attached_file :avatar, :styles => {:medium => "300x300", :pass => "260x180#", :thumb => "50x50#"},
+                    :path => ":rails_root/public/system/:attachment/:hash.:extension",
+                    :url => "/system/:attachment/:hash.:extension", :hash_secret => "longSecretString"
+
   name_regex = /\A[A-Z]\w+(-[A-Z]\w+)*\s(\w+\s)*[A-Z]\w+(-[A-Z]\w+)*\z/
 
   validates :name, :presence => true, :format => {:with => name_regex}
@@ -76,31 +76,31 @@ class User < ActiveRecord::Base
   validates :city, :presence => true
   validates :sex, :presence => true
   validates :address, :presence => true
-  
+
   def update_group_membership
     gapps = Gapps.new
-    gapps.add_group_member("leden", self.email, self.name.split[1], self.name.split[0]) if (self.email_changed? || self.user_type_id_changed?) && !self.email.empty? && (self.user_type_id == 1 || self.user_type_id == 2 || self.user_type == UserType.find_by_name("Proeflid"))
-    gapps.add_group_member("alleleden", self.email, self.name.split[1], self.name.split[0]) if self.email_changed? && !self.email.empty? && (self.user_type != UserType.find_by_name("Oudlid"))
+    gapps.add_group_member('leden', self.email, self.name.split[1], self.name.split[0]) if (self.email_changed? || self.user_type_id_changed?) && !self.email.empty? && (self.user_type_id == 1 || self.user_type_id == 2 || self.user_type == UserType.find_by_name("Proeflid"))
+    gapps.add_group_member('alleleden', self.email, self.name.split[1], self.name.split[0]) if self.email_changed? && !self.email.empty? && (self.user_type != UserType.find_by_name("Oudlid"))
   end
-  
+
   def purge_member_from_group
-     gapps = Gapps.new
-     gapps.destroy_group_member("leden", self.email_was) if (self.email_changed? || self.user_type_id_changed?) && !self.new_record? && (self.user_type_id_was == 1 || self.user_type_id_was == 2 || self.user_type == UserType.find_by_name("Proeflid"))
-     gapps.destroy_group_member("alleleden", self.email_was) if self.email_changed? && !self.new_record? && (self.user_type_was != UserType.find_by_name("Oudlid"))
-   end
-  
+    gapps = Gapps.new
+    gapps.destroy_group_member('leden', self.email_was) if (self.email_changed? || self.user_type_id_changed?) && !self.new_record? && (self.user_type_id_was == 1 || self.user_type_id_was == 2 || self.user_type == UserType.find_by_name("Proeflid"))
+    gapps.destroy_group_member('alleleden', self.email_was) if self.email_changed? && !self.new_record? && (self.user_type_was != UserType.find_by_name("Oudlid"))
+  end
+
   def add_member_to_group
     gapps = Gapps.new
-    gapps.add_group_member("leden", self.email, self.name.split[1], self.name.split[0]) if !self.email.empty? && (self.user_type_id == 1 || self.user_type_id == 2 || self.user_type == UserType.find_by_name("Proeflid"))
-    gapps.add_group_member("alleleden", self.email, self.name.split[1], self.name.split[0]) if !self.email.empty? && (self.user_type != UserType.find_by_name("Oudlid"))
+    gapps.add_group_member('leden', self.email, self.name.split[1], self.name.split[0]) if !self.email.empty? && (self.user_type_id == 1 || self.user_type_id == 2 || self.user_type == UserType.find_by_name('Proeflid'))
+    gapps.add_group_member('alleleden', self.email, self.name.split[1], self.name.split[0]) if !self.email.empty? && (self.user_type != UserType.find_by_name("Oudlid"))
   end
-  
+
   def remove_member_from_group
     gapps = Gapps.new
-    gapps.destroy_group_member("leden", self.email) if (self.user_type_id == 1 || self.user_type_id == 2 || self.user_type == UserType.find_by_name("Proeflid"))
-    gapps.destroy_group_member("alleleden", self.email) if (self.user_type != UserType.find_by_name("Oudlid"))
+    gapps.destroy_group_member('leden', self.email) if (self.user_type_id == 1 || self.user_type_id == 2 || self.user_type == UserType.find_by_name("Proeflid"))
+    gapps.destroy_group_member('alleleden', self.email) if (self.user_type != UserType.find_by_name('Oudlid'))
   end
-  
+
   def update_commission_email
     if self.email_changed?
       self.commission_memberships.each do |commem|
@@ -108,42 +108,42 @@ class User < ActiveRecord::Base
       end
     end
   end
-  
+
   def admin?
     self.commissions.each do |com|
-      if com.role == 'ADMIN' then
+      if com.role == 'ADMIN'
         return true
       end
     end
-	# voor backwards compatibility
+    # voor backwards compatibility
     self.commissions.exists?(Commission.find_by_name('Bestuur')) or self.commissions.exists?(Commission.find_by_name('Webcie'))
   end
-  
+
   def uitslagen_admin?
     self.commissions.each do |com|
-      if com.role == 'RESULT_ADMIN' then
+      if com.role == 'RESULT_ADMIN'
         return true
       end
     end
     false
   end
-  
+
   def kronometer_admin?
     self.commissions.each do |com|
-      if com.role == 'KRONOMETER_ADMIN' then
+      if com.role == 'KRONOMETER_ADMIN'
         return true
       end
     end
     false
   end
-  
+
   def active?
     self.commissions.size > 0
   end
-  
+
   def oudlid?
     if self.user_type
-      self.user_type.name == "Oudlid"
+      self.user_type.name == 'Oudlid'
     else
       false
     end
@@ -154,6 +154,5 @@ class User < ActiveRecord::Base
     bday += 1.year if Date.today >= bday
     (bday - Date.today).to_i
   end
-  
-end
 
+end
