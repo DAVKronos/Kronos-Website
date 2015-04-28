b9288af783db8e355edea5e90795904f51983df0
Wouter Timmermans <wouter@woutertimmermans.nl>
2015-04-28 22:01:29 +0200

    Mailinglist create, new en index

--- app/controllers/mailinglists_controller.rb
+++ app/controllers/mailinglists_controller.rb
@@ -6,12 +6,21 @@ class MailinglistsController < ApplicationController
   end
 
   def show
+    @mailinglist = Mailinglist.find(params[:id])
   end
 
   def new
+    @mailinglist = Mailinglist.new
   end
 
   def create
+    mailinglist = Mailinglist.new(params[:mailinglist])
+    if mailinglist.save
+      redirect_to mailinglist
+    else
+      @mailinglists = mailinglist
+      render 'new'
+    end
   end
 
   def destroy
--- spec/views/mailinglists/update.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "mailinglist/update.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
