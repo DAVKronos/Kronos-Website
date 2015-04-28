a720efdc4e8218db358a8da02bf98986e7af34d6
Wouter Timmermans <wouter@woutertimmermans.nl>
2015-04-28 22:41:53 +0200

    Mailinglist destroy

--- app/controllers/mailinglists_controller.rb
+++ app/controllers/mailinglists_controller.rb
@@ -24,6 +24,13 @@ class MailinglistsController < ApplicationController
   end
 
   def destroy
+    mailinglist = Mailinglist.find(params[:id])
+    if mailinglist.destroy
+      flash[:success] = t(:mailinglist_destroy_success)
+    else
+      flash[:error] = t(:mailinglist_destroy_failure)
+    end
+    redirect_to Mailinglist
   end
 
   def update
