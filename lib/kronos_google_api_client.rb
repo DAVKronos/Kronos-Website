441fedcc0abd70647ffe100a1b5260d646dfbe73
Wouter Timmermans <wouter@woutertimmermans.nl>
2015-05-03 21:43:08 +0200

    Test zou zonder error moeten draaien, fouten zou kunnen

--- lib/kronos_google_api_client.rb
+++ lib/kronos_google_api_client.rb
@@ -4,14 +4,17 @@ require 'google/api_client/auth/installed_app'
 
 class KronosGoogleAPIClient
 
-  # Initialize the client.
-  client = Google::APIClient.new(
-      :application_name => 'Kronos-Website',
-      :application_version => '1.0.0'
-  )
+  def initialize
+    # Initialize the client.
+    client = Google::APIClient.new(
+        :application_name => 'Kronos-Website',
+        :application_version => '1.0.0'
+    )
 
-# Load client secrets from your client_secrets.json.
-  client_secrets = Google::APIClient::ClientSecrets.load
+    # Load client secrets from your client_secrets.json.
+    client_secrets = Google::APIClient::ClientSecrets.load
+  end
+
 
 end
 
