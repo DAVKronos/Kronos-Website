00a0ae5b34f5b464263a1d2d4b3c15e7bdb662f7
Wouter Timmermans <wouter@woutertimmermans.nl>
2015-05-12 18:05:59 +0200

    Enable GC profiling

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# Enable Garbage collection profiling (used by newrelic)
+
+GC::Profiler.enable
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require *Rails.groups(:assets => %w(development test))
