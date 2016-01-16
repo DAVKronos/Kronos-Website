4d0108ad97f4e9c7313479cf50172867c17e526f
leon <leon_schenk@live.nl>
2016-01-16 22:10:55 +0100

    Verjaardagen niet van oud-leden

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,14 @@ class PagesController < ApplicationController
     @newsitems = Newsitem.where(:agreed => true).order('created_at DESC').limit(6)
     @agendaitems = Agendaitem.where("date >= ?", Time.now).order('date ASC').limit(10)
     
-    @birthdays = User.where('(extract(month from birthdate) = ? AND extract(day from birthdate) >= ?) OR
-                             (extract(month from birthdate) = ? AND extract(day from birthdate) <= ?)', 
-                             Date.today.strftime("%m"), Date.today.strftime("%d"),
-                             Date.today.next_month.strftime("%m"), Date.today.next_month.strftime("%d"))
+    @birthdays = User.where( '(user_type_id not in (?)) AND 
+                              (
+                               (extract(month from birthdate) = ? AND extract(day from birthdate) >= ?) OR
+                               (extract(month from birthdate) = ? AND extract(day from birthdate) <= ?)
+                              )', 
+                              [9], 
+                              Date.today.strftime("%m"), Date.today.strftime("%d"),
+                              Date.today.next_month.strftime("%m"), Date.today.next_month.strftime("%d"))
                      .order('extract(month from birthdate) ASC, extract(day from birthdate) ASC')
   end
 
