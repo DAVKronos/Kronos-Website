27cd33216285d92be9e47880df26cc220a2475ce
kronos <kronos@kronos.nl>
2016-04-25 17:07:39 +0200

    new database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151204172854) do
+ActiveRecord::Schema.define(:version => 20160425141720) do
 
   create_table "agendaitems", :force => true do |t|
     t.string   "name"
@@ -255,6 +255,8 @@ ActiveRecord::Schema.define(:version => 20151204172854) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "pagetag"
+    t.string   "menu"
+    t.boolean  "highlight"
   end
 
   create_table "photoalbums", :force => true do |t|
