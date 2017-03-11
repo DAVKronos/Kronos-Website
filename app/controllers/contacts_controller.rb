db187838f05bc87792b847c680d545c802f948d4
Wouter Timmermans <wouter@woutertimmermans.nl>
2017-03-11 20:18:32 +0100

    [#138585871] Send contact form e-mail to secretary, chairman and the sender

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,30 @@
 class ContactsController < ApplicationController
   load_and_authorize_resource
+  
+  CHAIRMAN_EMAIL = "voorzitter@kronos.nl"
+  SECRETARY_EMAIL = "secretaris@kronos.nl"
+  
   def new
     @contact = Contact.new
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @sender = params[:contact][:email]
+    @contact_params = params[:contact]
+    @contact_params[:destination] = @sender
+    @contact = Contact.new(@contact_params)
+    
+    @contact_params[:destination] = CHAIRMAN_EMAIL
+    @contact_chair = Contact.new(@contact_params)
+    
+    @contact_params[:destination] = SECRETARY_EMAIL
+    @contact_sec = Contact.new(@contact_params)
+    
+    
     @contact.request = request
-    if @contact.deliver
+    if @contact_chair.deliver
+      @contact.deliver
+      @contact_sec.deliver
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
       redirect_to :root
     else
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,16 @@
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :name,        :validate => true
+  attribute :email,       :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  attribute :nickname,  :captcha  => true
+  attribute :destination, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :nickname,    :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
         :subject => 'Bericht via Kronos.nl',
-        :to => 'voorzitter@kronos.nl',
+        :to => %(#{destination}),
         :from => %("#{name}" <#{email}>)
     }
   end
