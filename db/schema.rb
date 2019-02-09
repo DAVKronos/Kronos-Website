045558f40f8b3672b0e2153f9f47e589a1b01f6b
Rogier <r.monshouwer@student.utwente.nl>
2019-02-09 12:09:11 +0100

    Paperclip version same as master

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,14 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20181020212853) do
+ActiveRecord::Schema.define(version: 20190124102512) do
 
-  create_table "agendaitems", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "agendaitems", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "date"
@@ -28,46 +31,46 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.integer  "agendaitemtype_id"
     t.string   "url"
     t.integer  "user_id"
-    t.string   "name_en",              :default => "Agenda item"
-    t.text     "description_en",       :default => "English description not yet available"
+    t.string   "name_en",              default: "Agenda item"
+    t.text     "description_en",       default: "English description not yet available"
   end
 
-  create_table "agendaitemtype_eventtypes", :force => true do |t|
+  create_table "agendaitemtype_eventtypes", force: :cascade do |t|
     t.integer  "agendaitemtype_id"
     t.integer  "eventtype_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
-  create_table "agendaitemtypes", :force => true do |t|
+  create_table "agendaitemtypes", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean  "is_match"
     t.string   "name_en"
   end
 
-  create_table "aliases", :force => true do |t|
+  create_table "aliases", force: :cascade do |t|
     t.text     "name"
     t.text     "emailaddress"
     t.text     "description"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  create_table "aliases_mailinglists", :id => false, :force => true do |t|
+  create_table "aliases_mailinglists", id: false, force: :cascade do |t|
     t.integer "alias_id"
     t.integer "mailinglist_id"
   end
 
-  add_index "aliases_mailinglists", ["alias_id", "mailinglist_id"], :name => "index_aliases_mailinglists_on_alias_id_and_mailinglist_id"
+  add_index "aliases_mailinglists", ["alias_id", "mailinglist_id"], name: "index_aliases_mailinglists_on_alias_id_and_mailinglist_id", using: :btree
 
-  create_table "announcements", :force => true do |t|
+  create_table "announcements", force: :cascade do |t|
     t.text     "message"
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "background_file_name"
     t.string   "background_content_type"
     t.integer  "background_file_size"
@@ -76,15 +79,15 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.text     "url"
   end
 
-  create_table "apps_exceptions", :force => true do |t|
+  create_table "apps_exceptions", force: :cascade do |t|
     t.string   "email"
     t.boolean  "way"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "group_email"
   end
 
-  create_table "chatmessages", :force => true do |t|
+  create_table "chatmessages", force: :cascade do |t|
     t.string   "by"
     t.text     "message"
     t.datetime "created_at"
@@ -96,9 +99,9 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.string   "referrer"
   end
 
-  create_table "comments", :force => true do |t|
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "user_id"
     t.integer  "agendaitem_id"
     t.integer  "result_id"
@@ -109,16 +112,16 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.string   "commentable_type"
   end
 
-  create_table "commission_memberships", :force => true do |t|
+  create_table "commission_memberships", force: :cascade do |t|
     t.string   "function"
-    t.boolean  "installed",     :default => false
-    t.integer  "user_id",                          :null => false
-    t.integer  "commission_id",                    :null => false
+    t.boolean  "installed",     default: false
+    t.integer  "user_id",                       null: false
+    t.integer  "commission_id",                 null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "commissions", :force => true do |t|
+  create_table "commissions", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "email"
@@ -129,9 +132,9 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.text     "description_en"
   end
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0
+    t.integer  "attempts",   default: 0
     t.text     "handler"
     t.text     "last_error"
     t.datetime "run_at"
@@ -143,9 +146,9 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.string   "queue"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
-  create_table "events", :force => true do |t|
+  create_table "events", force: :cascade do |t|
     t.time     "date"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -154,7 +157,7 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.float    "distance"
   end
 
-  create_table "eventtypes", :force => true do |t|
+  create_table "eventtypes", force: :cascade do |t|
     t.string   "name"
     t.string   "formula"
     t.string   "shortname"
@@ -162,42 +165,43 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "calculated_unit"
-    t.boolean  "show_wind",       :default => false
+    t.boolean  "show_wind",       default: false
     t.string   "female_formula"
+    t.boolean  "important"
   end
 
-  create_table "kronometers", :force => true do |t|
+  create_table "kronometers", force: :cascade do |t|
     t.string   "name"
     t.date     "date"
     t.string   "file_file_name"
     t.string   "file_content_type"
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
-  create_table "mailinglist_memberships", :force => true do |t|
+  create_table "mailinglist_memberships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "mailinglist_id"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
-  add_index "mailinglist_memberships", ["mailinglist_id"], :name => "index_mailinglist_memberships_on_mailinglist_id"
-  add_index "mailinglist_memberships", ["user_id"], :name => "index_mailinglist_memberships_on_user_id"
+  add_index "mailinglist_memberships", ["mailinglist_id"], name: "index_mailinglist_memberships_on_mailinglist_id", using: :btree
+  add_index "mailinglist_memberships", ["user_id"], name: "index_mailinglist_memberships_on_user_id", using: :btree
 
-  create_table "mailinglists", :force => true do |t|
+  create_table "mailinglists", force: :cascade do |t|
     t.text     "name"
     t.text     "description"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "local_part"
     t.integer  "commission_id"
     t.integer  "mailinglist_id"
   end
 
-  create_table "newsitems", :force => true do |t|
+  create_table "newsitems", force: :cascade do |t|
     t.string   "title"
     t.text     "news"
     t.boolean  "agreed"
@@ -209,55 +213,55 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.string   "articlephoto_content_type"
     t.integer  "articlephoto_file_size"
     t.datetime "articlephoto_updated_at"
-    t.string   "title_en",                  :default => "News item"
-    t.text     "news_en",                   :default => "English text not yet available"
+    t.string   "title_en",                  default: "News item"
+    t.text     "news_en",                   default: "English text not yet available"
   end
 
-  create_table "old_photoalbums", :id => false, :force => true do |t|
-    t.integer "FotoAlbumID",                :null => false
-    t.string  "Naam",        :limit => 150
+  create_table "old_photoalbums", id: false, force: :cascade do |t|
+    t.integer "FotoAlbumID",             null: false
+    t.string  "Naam",        limit: 150
     t.date    "Datum"
   end
 
-  create_table "old_photos", :id => false, :force => true do |t|
-    t.integer "FotoID",       :null => false
+  create_table "old_photos", id: false, force: :cascade do |t|
+    t.integer "FotoID",       null: false
     t.integer "FotoAlbumID"
     t.text    "Omschrijving"
     t.integer "LidID"
   end
 
-  create_table "old_users", :id => false, :force => true do |t|
-    t.integer "LidID",                              :null => false
-    t.string  "Voorletters",         :limit => 12
-    t.string  "Voornaam",            :limit => 50
-    t.string  "Achternaam",          :limit => 60
-    t.string  "Naam",                :limit => 70
+  create_table "old_users", id: false, force: :cascade do |t|
+    t.integer "LidID",                           null: false
+    t.string  "Voorletters",         limit: 12
+    t.string  "Voornaam",            limit: 50
+    t.string  "Achternaam",          limit: 60
+    t.string  "Naam",                limit: 70
     t.date    "GeboorteDatum"
-    t.string  "Email",               :limit => 100
-    t.string  "Adres",               :limit => 100
-    t.string  "Postcode",            :limit => 10
-    t.string  "Woonplaats",          :limit => 80
-    t.string  "Wijk",                :limit => 20
-    t.string  "UIN",                 :limit => 20
-    t.string  "Telefoon",            :limit => 30
-    t.string  "MobieleTelefoon",     :limit => 30
+    t.string  "Email",               limit: 100
+    t.string  "Adres",               limit: 100
+    t.string  "Postcode",            limit: 10
+    t.string  "Woonplaats",          limit: 80
+    t.string  "Wijk",                limit: 20
+    t.string  "UIN",                 limit: 20
+    t.string  "Telefoon",            limit: 30
+    t.string  "MobieleTelefoon",     limit: 30
     t.text    "Opmerking"
     t.integer "Geslacht"
-    t.string  "Wachtwoord",          :limit => 15
-    t.string  "LicentieNummer",      :limit => 25
+    t.string  "Wachtwoord",          limit: 15
+    t.string  "LicentieNummer",      limit: 25
     t.integer "LidTypeID"
     t.integer "Privacy"
     t.integer "Incasso"
     t.integer "Jurylid"
-    t.string  "Campuskaart",         :limit => 20
-    t.string  "Rekeningnummer",      :limit => 20
-    t.string  "WedstrijdVereniging", :limit => 80
-    t.string  "Nickname",            :limit => 100
+    t.string  "Campuskaart",         limit: 20
+    t.string  "Rekeningnummer",      limit: 20
+    t.string  "WedstrijdVereniging", limit: 80
+    t.string  "Nickname",            limit: 100
     t.integer "OVKaart"
     t.integer "KMeterPapier"
   end
 
-  create_table "pages", :force => true do |t|
+  create_table "pages", force: :cascade do |t|
     t.text     "information"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -268,7 +272,7 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.text     "information_en"
   end
 
-  create_table "photoalbums", :force => true do |t|
+  create_table "photoalbums", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -276,7 +280,7 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.boolean  "public"
   end
 
-  create_table "photos", :force => true do |t|
+  create_table "photos", force: :cascade do |t|
     t.string   "caption"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -290,7 +294,7 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.datetime "exif_date"
   end
 
-  create_table "results", :force => true do |t|
+  create_table "results", force: :cascade do |t|
     t.string   "username"
     t.string   "result"
     t.datetime "created_at"
@@ -302,7 +306,7 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.decimal  "calculated"
   end
 
-  create_table "subscriptions", :force => true do |t|
+  create_table "subscriptions", force: :cascade do |t|
     t.text     "comment"
     t.string   "name"
     t.datetime "created_at"
@@ -311,7 +315,7 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.integer  "agendaitem_id"
   end
 
-  create_table "tags", :force => true do |t|
+  create_table "tags", force: :cascade do |t|
     t.integer  "photo_id"
     t.integer  "user_id"
     t.integer  "xpos"
@@ -322,41 +326,41 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.datetime "updated_at"
   end
 
-  create_table "user_sessions", :force => true do |t|
+  create_table "user_sessions", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "user_types", :force => true do |t|
+  create_table "user_types", force: :cascade do |t|
     t.string   "name"
     t.boolean  "donor"
     t.boolean  "competition"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "initials"
-    t.string   "email",                  :default => "",    :null => false
+    t.string   "email",                  default: "",    null: false
     t.date     "birthdate"
     t.string   "address"
     t.string   "postalcode"
     t.string   "city"
     t.string   "sex"
     t.string   "licensenumber"
-    t.boolean  "papieren_kronometer",    :default => false
+    t.boolean  "papieren_kronometer",    default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -371,18 +375,18 @@ ActiveRecord::Schema.define(:version => 20181020212853) do
     t.integer  "aanvang"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "versions", :force => true do |t|
-    t.string   "item_type",  :null => false
-    t.integer  "item_id",    :null => false
-    t.string   "event",      :null => false
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
   end
 
-  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
 end
