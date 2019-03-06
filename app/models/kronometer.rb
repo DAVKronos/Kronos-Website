31f44ec5f59222b77032136dd4ec58026536af1d
Rogier <r.monshouwer@student.utwente.nl>
2019-03-06 12:04:29 +0100

    Bug fix: Validation of kronometer file

--- app/models/kronometer.rb
+++ app/models/kronometer.rb
@@ -17,4 +17,5 @@ class Kronometer < ActiveRecord::Base
   attr_accessible :date, :file, :name
   
   has_attached_file :file, :styles => { :thumb => ["260", :png] }
+  do_not_validate_attachment_file_type :file
 end
