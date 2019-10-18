a806c41629890f90fdbb48515e9ec5201a7b8934
Vincetn Bosboom <v.bosboom@student.utwente.nl>
2019-10-18 17:49:48 +0200

    revert minor gem changes

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module KronosWebsite
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :nl
     
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
