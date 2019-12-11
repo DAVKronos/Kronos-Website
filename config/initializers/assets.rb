963ca9d928620d1c3a92f686003ec738464358c2
Vincent Bosboom <v.bosboom@student.utwente.nl>
2019-12-11 18:49:28 +0100

    maybe now?

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( alphabetical_paginate.js )
+Rails.application.config.assets.precompile += %w( helpers.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
