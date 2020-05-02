fcad9760bc472dcd4a5fac7faddb5d00effa5cab
Rogier <r.monshouwer@student.utwente.nl>
2020-05-02 15:00:18 +0200

    Small fix for local development

--- app/models/abilities/authenticated.rb
+++ app/models/abilities/authenticated.rb
@@ -15,9 +15,11 @@ module Abilities
         if !(Agendaitem.find(subs.agendaitem_id).deadline_passed?)
           can :destroy, subs
 		end
-      end
-      Result.all.where(event_id:  Event.find_by(agendaitem_id: Agendaitem.find_by(name: 'Pilscie Games').id).id).where(user_id: user.id).each do |res|
-        can :destroy, res
+    end
+      if Agendaitem.find_by(name: 'Pilscie Games')
+        Result.all.where(event_id:  Event.find_by(agendaitem_id: Agendaitem.find_by(name: 'Pilscie Games').id).id).where(user_id: user.id).each do |res|
+          can :destroy, res
+        end
       end
       can [:update, :editpassword], user
 
