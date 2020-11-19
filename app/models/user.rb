b8cffa3eab21138dc2c88e7a2174468a43b2f82d
Rogier <r.monshouwer@student.utwente.nl>
2020-11-19 23:13:21 +0100

    Implemented a change email address function

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,7 +66,7 @@ class User < ApplicationRecord
   validates :address, :presence => true
   # Dit vereenvoudigt de callback functies voor de maillijst
   validates :email, :presence => true
-  after_update :change_mailinglist_email!, :if => :saved_changes_to_email?
+  after_update :change_mailinglist_email, :if => :saved_change_to_email?
 
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
