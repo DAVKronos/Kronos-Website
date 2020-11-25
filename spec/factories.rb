cc8c752e48714a01fc1d992815b7725b82e0bc54
Rogier <r.monshouwer@student.utwente.nl>
2020-11-25 18:54:59 +0100

    Get rid of rubocup deprecation warnings

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,20 +2,20 @@
 
 FactoryBot.define do
 	factory :user do
-	  name                  "Wouter Timmermans"
-	  email                 "wouter@example.com"
-	  address               "Geenstraat 3"
-	  city                  "Enschede"
-	  initials              "A.B.C."
-	  birthdate             "01-01-2000"
-	  postalcode            "1234 AB"
-	  licensenumber         "123456"
-	  sex                   "Man"
-	  password              "foobar"
-	  password_confirmation "foobar"
+	  name                  {"Wouter Timmermans"}
+	  email                 {"wouter@example.com"}
+	  address               {"Geenstraat 3"}
+	  city                  {"Enschede"}
+	  initials              {"A.B.C."}
+	  birthdate             {"01-01-2000"}
+	  postalcode            {"1234 AB"}
+	  licensenumber         {"123456"}
+	  sex                   {"Man"}
+	  password              {"foobar"}
+	  password_confirmation {"foobar"}
 		
 		factory :admin do
-			email	"woutertimmermans@kronos.nl"
+			email	{"woutertimmermans@kronos.nl"}
 			after(:create) do |admin|
 				commission = FactoryBot.create(:bestuur)
 				FactoryBot.create(:commission_membership, user: admin, commission: commission)
@@ -24,21 +24,21 @@ FactoryBot.define do
 	end
 	
 	factory :commission_membership do
-		function "Voorzitter"
+		function {"Voorzitter"}
 		user
 		commission
 	end
 	
 	factory :bestuur, class: Commission do
-		name					"Bestuur"
-		description 	"Het bestuur van Kronos"
-		email 				"bestuur@kronos.nl"
+		name					{"Bestuur"}
+		description { "}Het bestuur van Kronos"}
+		email 				{"bestuur@kronos.nl"}
 	end
 		
 	factory :commission do
-	  name           "Test"
-	  description    "Test Commission"
-	  email          "test@kronos.nl"
+	  name           {"Test"}
+	  description    {"Test Commission"}
+	  email          {"test@kronos.nl"}
 	end
 end
 
