069af237bcbeb4bba39e0afaf947b95a5f36d0d1
rogiermonshouwer <rogier2012@icloud.com>
2021-06-07 20:32:39 +0200

    Correct static images in production

--- packs/application.js
+++ packs/application.js
@@ -2,4 +2,5 @@
 var componentRequireContext = require.context("src", true);
 var ReactRailsUJS = require("react_ujs");
 import '../src/App.scss';
+const images = require.context('src/images', true)
 ReactRailsUJS.useContext(componentRequireContext);
--- src/components/Header/LanguageSwitch.js
+++ src/components/Header/LanguageSwitch.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
 import {Nav} from 'react-bootstrap';
+import flagNL from '../../images/Site_EN.png';
+import flagEN from '../../images/Site_NL.png';
 
 const LanguageSwitch = () => {
     const {i18n} = useTranslation();
@@ -8,8 +10,10 @@ const LanguageSwitch = () => {
     if (i18n.language.indexOf('en') >= 0) {
         nextLanguage = 'nl'
     }
+
+    const image = nextLanguage === 'en' ? flagEN : flagNL;
     return <Nav.Link className='language-switch' onClick={() => i18n.changeLanguage(nextLanguage)}>
-        <img style={{minWidth: 12}} src={`../images/Site_${nextLanguage.toUpperCase()}.png`}/>
+        <img style={{minWidth: 12}} src={image}/>
     </Nav.Link>
 }
 
