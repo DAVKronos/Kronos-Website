0ae43a2a71f56009432a8805fee15b5abac4475f
rogiermonshouwer <rogier2012@icloud.com>
2021-07-23 14:57:50 +0200

    Improved loading of photoalbums - added generic pagination component - added pagination to photoalbum and photos - added placeholder

--- app/javascript/src/components/PhotoAlbums/PhotoAlbums.js
+++ app/javascript/src/components/PhotoAlbums/PhotoAlbums.js
@@ -2,30 +2,27 @@ import React from 'react';
 import {Button, Col, Row} from 'react-bootstrap';
 import PhotoAlbumCover from "./PhotoAlbumCover";
 import DefaultSpinner from "../Generic/Spinner";
+import Paginate from "../Generic/Paginate";
 import {useQuery} from "react-query";
-import {getPhotoAlbums, removePhotoAlbum} from "./queries";
+import {getPhotoAlbums} from "./queries";
 import {Link} from "react-router-dom";
 import {Can} from "../../utils/auth-helper";
 import {useTranslation} from "react-i18next";
 
 function PhotoAlbums(props) {
-    const { isLoading, isError, data, error } = useQuery('photoalbums', getPhotoAlbums);
+    const { isLoading, isError, data: photoAlbums, error } = useQuery('photoalbums', getPhotoAlbums);
     const {t} = useTranslation('generic');
-    const photoAlbums = data;
 
+    const Item = ({item}) => <PhotoAlbumCover photoAlbum={item}/>
 
     return <React.Fragment>
         <Row>
             <Col><h1>{t('photoAlbumPage:header')}</h1></Col>
         </Row>
-        <Row>
-            {isLoading && <DefaultSpinner/>}
-            {photoAlbums && photoAlbums.map(photoAlbum => {
-                return <Col key={photoAlbum.id} sm={6} md={4}>
-                    <PhotoAlbumCover photoAlbum={photoAlbum}/>
-                </Col>
-            })}
-        </Row>
+
+        {isLoading && <DefaultSpinner/>}
+        {photoAlbums && photoAlbums.length > 0 && <Paginate data={photoAlbums} itemsPerPage={8} colSize={{sm: 6, md:3}} Item={Item} />}
+
         <Can I='create' a='Photoalbum'>
             <Button as={Link} to='/photoalbums/new'>{t('addModel', {model: t('models:modelNames.photoAlbum')})}</Button>
         </Can>
