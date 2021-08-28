6ddec519918d61dfd139fa3da47559d10028b4c8
rogiermonshouwer <rogier2012@icloud.com>
2021-08-28 08:31:46 +0200

    First implementation comments

--- src/components/NewsItems/NewsItem.js
+++ src/components/NewsItems/NewsItem.js
@@ -1,14 +1,54 @@
 import React from "react";
-import {Button, Col, Row} from 'react-bootstrap';
+import {Button, Col, Row, Image} from 'react-bootstrap';
 import {getAPIHostUrl} from "../../utils/rest-helper";
 import {format} from '../../utils/date-format';
-import {useQuery} from "react-query";
-import {getNewsItem, removeNewsItem} from "./queries";
+import {useQuery, useQueryCache} from "react-query";
+import {getComments, getNewsItem, removeComment, removeNewsItem} from "./queries";
 import DefaultSpinner from "../Generic/Spinner";
 import {Can} from "../../utils/auth-helper";
 import {useTranslation} from "react-i18next";
 import {Link, useHistory} from "react-router-dom";
 import ReactMarkdown from 'react-markdown';
+import {getUser} from "../Users/queries";
+import {subject} from "@casl/ability";
+
+const NewsItemComments = ({newsitemId}) => {
+    const { isLoading, isError, data: comments, error } = useQuery(['comments',newsitemId], getComments);
+    
+    return <div>
+        {comments && comments.map(comment => {
+            return <Comment key={comment.id} comment={comment}/>;
+        })
+        }
+    </div>;
+}
+
+const Comment = ({comment}) => {
+    const { isLoading, isError, data: user, error } = useQuery(['users',comment.user_id], getUser);
+    const {t} = useTranslation('generic');
+    const queryCache = useQueryCache()
+    const onClickRemove = () => {
+        removeComment(comment.commentable_id, comment.id).then(() => {
+            queryCache.invalidateQueries(['comments', comment.commentable_id]);
+        });
+    }
+    return <Row style={{borderTop: "1px solid #eee", paddingTop: 10}}>
+        <Col md={2} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+            {isLoading && <DefaultSpinner/>}
+            {user && <Image src={user.avatar_url_thumb} roundedCircle />}
+            {user && <h5>{user.name}</h5>}
+        </Col>
+        <Col md={8} style={{display: 'flex', alignItems: 'center'}}>
+            {comment.commenttext}
+        </Col>
+        <Col md={2}>
+            <Can I={'delete'} this={subject('Comment', comment)}>
+                <Button variant='danger' onClick={onClickRemove}>{t('remove')}</Button>
+            </Can>
+        </Col>
+    </Row>
+}
+
 
 
 function NewsItem(props) {
@@ -45,6 +85,9 @@ function NewsItem(props) {
             <Col md={{span: 8, offset: 2}}>
                 <img src={getAPIHostUrl(item.articlephoto_url_carrousel)} alt={item.title}/>
                 <ReactMarkdown source={news}/>
+                <Can I='read' a={'Comment'}>
+                    {item && <NewsItemComments newsitemId={item.id} />}
+                </Can>
             </Col>
             <Col md={2}>
                 <Can I="update" a={"Newsitem"}>
--- src/components/NewsItems/queries.js
+++ src/components/NewsItems/queries.js
@@ -5,6 +5,10 @@ function getNewsItem(queryKey, id) {
     return restCall(`newsitems/${id}`).then(res => res.data)
 }
 
+function getComments(queryKey, newsItemId) {
+    return restCall(`newsitems/${newsItemId}/comments`).then(res => res.data);
+}
+
 function createNewsItem(data) {
     const formData = convertToFormData('newsitem', data);
     return restCall(`newsitems/`, {method: 'POST', data: formData}).then(res => res.data);
@@ -19,4 +23,8 @@ function removeNewsItem(id) {
     return restCall(`newsitems/${id}`, {method: 'DELETE'});
 }
 
-export {getNewsItem, createNewsItem, updateNewsItem, removeNewsItem}
+function removeComment(newsItemId, id) {
+    return restCall(`newsitems/${newsItemId}/comments/${id}`, {method: 'DELETE'});
+}
+
+export {getNewsItem, getComments, createNewsItem, updateNewsItem, removeNewsItem, removeComment}
