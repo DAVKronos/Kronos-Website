c226b3b003ec993de4d8c89f3694c1d89b8ae996
rogiermonshouwer <rogier2012@icloud.com>
2021-09-20 22:11:03 +0200

    Mobile improvements

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {NavLink} from 'react-router-dom';
 import UserMenu from "./LoginMenu";
@@ -16,7 +16,7 @@ function getPagesForMenu(pages, title) {
 
 function getPageLinksForMenu(pages, title) {
     return getPagesForMenu(pages, title).map(page => {
-        return <NavDropdown.Item key={page.pagetag} as={NavLink} to={`/${page.pagetag}`}>{page.pagetag}</NavDropdown.Item>
+        return <NavDropdown.Item key={page.pagetag} as={NavLink} to={`/${page.pagetag}`} href={`/${page.pagetag}`}>{page.pagetag}</NavDropdown.Item>
     })
 }
 
@@ -24,7 +24,7 @@ function getHighlightPages(pages) {
     return pages.filter((page) => {
         return page.highlight;
     }).map(page => {
-        return <Nav.Link className='highlight' key={page.pagetag} as={NavLink} to={`/${page.pagetag}`}>{page.pagetag}</Nav.Link>;
+        return <Nav.Link className='highlight' key={page.pagetag} as={NavLink} to={`/${page.pagetag}`} href={`/${page.pagetag}`}>{page.pagetag}</Nav.Link>;
     })
 }
 
@@ -32,16 +32,16 @@ const NavBar =  () => {
     const { isLoading, isError, data } = useQuery('pages', getPages);
     const { t } = useTranslation('navigation');
     const pages = data || [];
-    return <Navbar id="navbar-kronos" expand="lg">
+    return <Navbar id="navbar-kronos" expand="lg" variant="dark" collapseOnSelect>
         <Navbar.Brand className="d-md-none d-flex">Kronos</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav" className="flex-wrap">
-            <Nav>
-                <Nav.Link as={NavLink} to="/" exact>Home</Nav.Link>
+            <Nav >
+                <Nav.Link as={NavLink} to="/" href={'/'} exact>Home</Nav.Link>
                 <NavDropdown title={t('association')} id="basic-nav-dropdown">
-                    <NavDropdown.Item as={NavLink} to="/commissions">{t('committees')}</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/commissions" href="/commissions">{t('committees')}</NavDropdown.Item>
                     {getPageLinksForMenu(pages,'Vereniging')}
-                    <NavDropdown.Item as={NavLink} to="/kronometers">{t('kronometers')}</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/kronometers" href="/kronometers">{t('kronometers')}</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title={t('trainings')} id="basic-nav-dropdown">
                     {getPageLinksForMenu(pages,'Trainingen')}
@@ -49,10 +49,10 @@ const NavBar =  () => {
                 <NavDropdown title={t('matches')} id="basic-nav-dropdown">
                     {getPageLinksForMenu(pages, 'Wedstrijden')}
                 </NavDropdown>
-                <Nav.Link as={NavLink} to="/agendaitems">{t('agenda')}</Nav.Link>
-                <Nav.Link as={NavLink} to="/results">{t('results')}</Nav.Link>
-                <Nav.Link as={NavLink} to="/photoalbums">{t('photos')}</Nav.Link>
-                <Nav.Link as={NavLink} to="/contact">{t('contact')}</Nav.Link>
+                <Nav.Link as={NavLink} to="/agendaitems" href="/agendaitems">{t('agenda')}</Nav.Link>
+                <Nav.Link as={NavLink} to="/results" href="/results">{t('results')}</Nav.Link>
+                <Nav.Link as={NavLink} to="/photoalbums" href="/photoalbums">{t('photos')}</Nav.Link>
+                <Nav.Link as={NavLink} to="/contact" href="/contact">{t('contact')}</Nav.Link>
 
             </Nav>
             <Nav>
