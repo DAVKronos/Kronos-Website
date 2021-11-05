a80e3748c44d10f94182d6f95844277cf0dcb936
rogiermonshouwer <rogier2012@icloud.com>
2021-11-05 20:48:49 +0100

    Edit information pages

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import {Switch, Route} from 'react-router-dom';
 import {Page, PageWithTag} from "./Page";
+import PrivateRoute from '../Generic/PrivateRoute';
+import EditPage from './EditPage';
+
 
 // TODO PageId should be accesed by id or pagetag.
 const PhotoAlbumsRouter = () => {
     return <Switch>
+        <PrivateRoute path='/pages/:id/edit' component={EditPage} action={'update'} subject='Page'/>
         <Route path="/pages/:id" component={Page} />
         <Route path="/:pagetag"  render={(routeProps) => {
             // Key is to rerender when switching to a new page
--- src/components/Pages/queries.js
+++ src/components/Pages/queries.js
@@ -1,9 +1,14 @@
-import {restCall} from "../../utils/rest-helper";
+import {restCall, convertToFormData} from "../../utils/rest-helper";
 
 
 function getPage(queryKey, id) {
     return restCall(`pages/${id}`).then(res => res.data)
 }
+
+function getPages(queryKey) {
+    return restCall('pages').then(res => res.data)
+}
+
 
 function getPageByPageTag(queryKey, pagetag) {
     return restCall('pages').then(res => {
@@ -11,4 +16,18 @@ function getPageByPageTag(queryKey, pagetag) {
     });
 }
 
-export {getPage, getPageByPageTag}
+function createPage(data) {
+    const formData = convertToFormData('page', data);
+    return restCall(`pages/`, {method: 'POST', data: formData}).then(res => res.data);
+}
+
+function updatePage(id, data) {
+    const formData = convertToFormData('page', data);
+    return restCall(`pages/${id}`, { method: 'PUT', data: formData }).then(res => res.data);;
+}
+
+function removePage(id) {
+    return restCall(`pages/${id}`, {method: 'DELETE'});
+}
+
+export {getPage, getPages, getPageByPageTag, createPage, updatePage, removePage}
