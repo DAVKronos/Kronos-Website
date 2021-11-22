95f268542a72af98b0bba54d8a58bb3d24f22f15
rogiermonshouwer <rogier2012@icloud.com>
2021-11-22 21:47:31 +0100

    New page form + page fixes

--- app/javascript/src/components/Pages/index.js
+++ app/javascript/src/components/Pages/index.js
@@ -3,11 +3,13 @@ import {Switch, Route} from 'react-router-dom';
 import {Page, PageWithTag} from "./Page";
 import PrivateRoute from '../Generic/PrivateRoute';
 import EditPage from './EditPage';
+import NewPage from './NewPage';
 
 
 // TODO PageId should be accesed by id or pagetag.
 const PhotoAlbumsRouter = () => {
     return <Switch>
+        <PrivateRoute path='/pages/new' component={NewPage} action={'create'} subject='Page'/>
         <PrivateRoute path='/pages/:id/edit' component={EditPage} action={'update'} subject='Page'/>
         <Route path="/pages/:id" component={Page} />
         <Route path="/:pagetag"  render={(routeProps) => {
