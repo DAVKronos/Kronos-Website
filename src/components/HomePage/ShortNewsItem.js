d58baf2f46b447d62d315b88c28c1c73d35b5841
rogiermonshouwer <rogier2012@icloud.com>
2021-11-22 22:28:04 +0100

    Add comment count

--- src/components/HomePage/ShortNewsItem.js
+++ src/components/HomePage/ShortNewsItem.js
@@ -5,12 +5,18 @@ import {getAPIHostUrl} from "../../utils/rest-helper";
 import {format} from '../../utils/date-format.js'
 import {useTranslation} from "react-i18next";
 import ReactMarkdown from "react-markdown";
+import {BsFillChatTextFill} from 'react-icons/bs';
 
 const ShortNewsItem = ({item}) => {
     const { t, i18n } = useTranslation('homepage');
     const title = i18n.language === 'nl' ? item.title : item.title_en;
     let news = i18n.language === 'nl' ? item.news : item.news_en;
     news = news.split('\n')[0];
+
+    let commentCount = null;
+    if (item.comment_count) {
+        commentCount = <React.Fragment> | <BsFillChatTextFill/> {item.comment_count}</React.Fragment>
+    }
 
     return <Row>
         <Col md={3} style={{display: 'flex'}}>
@@ -21,7 +27,7 @@ const ShortNewsItem = ({item}) => {
         <Col md={9}>
             <header>
                 <Link to={`/newsitems/${item.id}`}><h2>{title}</h2></Link>
-                <p>{format(item.created_at, 'PPP p', i18n.language)} | {item.user.name}</p>
+                <p>{format(item.created_at, 'PPP p', i18n.language)} | {item.user.name}{commentCount}</p>
             </header>
             <ReactMarkdown>{news}</ReactMarkdown>
             <Link to={`/newsitems/${item.id}`}>{t('readMore')}</Link>
