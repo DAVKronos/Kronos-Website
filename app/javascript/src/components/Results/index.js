2cdab017b286fa6171d8a31025a4fdbcf5cfafb4
Vincent Bosboom <v.bosboom@student.utwente.nl>
2021-12-28 15:55:34 +0100

    Add clubrecord page

--- app/javascript/src/components/Results/index.js
+++ app/javascript/src/components/Results/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Switch, Route} from 'react-router-dom';
 import Results from "./Results";
+import Records from "./Records";
 
 const ResultsRouter = () => {
     return <Switch>
-        <Route exact path="/results" component={Results}>
-
-        </Route>
+        <Route exact path="/results" component={Results} />
+        <Route path="/results/records" component={Records} />
     </Switch>
 };
 
