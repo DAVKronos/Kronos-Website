db64eaa3fe9d019ec0b4e098500e27a566bc7ea5
Vincent Bosboom <v.bosboom@student.utwente.nl>
2022-01-28 15:19:43 +0100

    banner attachment validation

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,7 +4,7 @@ class Announcement < ApplicationRecord
           :path        => ":rails_root/public/system/:attachment/:hash.:extension",
           :url         => "/system/:attachment/:hash.:extension", 
           :hash_secret => "longSecretString"
-
+  validates_attachment_content_type :background, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   def self.current(hidden_ids = nil)
     result = where("starts_at <= :now and ends_at >= :now", now: Time.zone.now)
     result = result.where("id not in (?)", hidden_ids) if hidden_ids.present?
