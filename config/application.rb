4f6bcb8f73fb372949014df3b7c2c6f189afeb28
Maurice van Leeuwen <maurice.vanleeuwen@gmail.com>
2022-05-31 15:16:35 +0200

    Migration to Rails 6.0.5

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module KronosWebsite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     config.time_zone = 'Europe/Amsterdam'
     config.i18n.default_locale = :nl
