5ca71dbd4cc944357d0e18b08779f5b3587ff2e7
TwanBerg <t.berg@student.utwente.nl>
2022-10-18 22:15:07 +0200

    Implemented aanpassen pages

--- app/javascript/src/components/Pages/Page.js
+++ app/javascript/src/components/Pages/Page.js
@@ -1,44 +1,46 @@
 import React from "react";
 import ReactMarkdown from 'react-markdown';
 import Spinner from "../Generic/Spinner";
-import {useQuery} from "react-query";
-import {getPage, getPageByPageTag} from "./queries";
+import { useQuery } from "react-query";
+import { getPage, getPageByPageTag } from "./queries";
 import MultiLanguageText from "../Generic/MultiLanguageText";
+import { Link } from "react-router-dom";
 
-const PageComponent = ({page, isLoading}) => {
-    if (isLoading) {
-        return <Spinner />;
-    }
+const PageComponent = ({ page, isLoading }) => {
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-    if (!page) {
-        return <h1>Page not found</h1>;
-    }
+  if (!page) {
+    return <h1>Page not found</h1>;
+  }
 
-    const renderMarkdown = (text) => {
-        text = text.replace("kronos.nl/", "kronos.nl/app/")
-        return <ReactMarkdown source={text} escapeHtml={false}/>;
-    }
+  const renderMarkdown = (text) => {
+    text = text.replace("kronos.nl/", "kronos.nl/app/")
+    return <ReactMarkdown source={text} escapeHtml={false} />;
+  }
 
-    return <div>
-        <h1><MultiLanguageText nl={page.pagetag} en={page.pagetag_en}/></h1>
-        <MultiLanguageText nl={page.information} en={page.information_en} renderFunction={renderMarkdown} />
-    </div>;
+  return <div>
+    <Link to={`/pages/${page.id}/edit`}>Aanpassen</Link>
+    <h1><MultiLanguageText nl={page.pagetag} en={page.pagetag_en} /></h1>
+    <MultiLanguageText nl={page.information} en={page.information_en} renderFunction={renderMarkdown} />
+  </div>;
 }
 
 
 const Page = (props) => {
-    let {id} = props.match.params;
-    const { isLoading, isError, data, error } = useQuery(['pages', id], getPage)
+  let { id } = props.match.params;
+  const { isLoading, isError, data, error } = useQuery(['pages', id], getPage)
 
-    return <PageComponent page={data} isLoading={isLoading}/>;
+  return <PageComponent page={data} isLoading={isLoading} />;
 }
 
 const PageWithTag = (props) => {
-    let {pagetag} = props.match.params;
-    const { isLoading, isError, data, error } = useQuery(['pages', pagetag], getPageByPageTag)
+  let { pagetag } = props.match.params;
+  const { isLoading, isError, data, error } = useQuery(['pages', pagetag], getPageByPageTag)
 
-    return <PageComponent page={data} isLoading={isLoading}/>;
+  return <PageComponent page={data} isLoading={isLoading} />;
 }
 
 
-export {Page, PageWithTag}
+export { Page, PageWithTag }
