10aaf728902e033d2501023740e08b667ad6cbaf
Maurice van Leeuwen <maurice.vanleeuwen@gmail.com>
2022-11-15 21:11:01 +0100

    Clean up old site

--- packs/server_rendering.js
+++
@@ -1,8 +0,0 @@
-// By default, this pack is loaded for server-side rendering.
-// It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
-var componentRequireContext = require.context("src", true);
-var ReactRailsUJS = require("react_ujs");
-import '../src/App';
-import '../src/App.scss';
-const images = require.context('src/images', true)
-ReactRailsUJS.useContext(componentRequireContext);
