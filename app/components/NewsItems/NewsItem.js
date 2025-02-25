8863fcb4e22f230f687eb124c6641c2c5c69535c
Wessel Witteveen <witteveenwessel@gmail.com>
2025-02-25 18:41:50 +0100

    Changed comment structure to remove user api call and use the returned user

--- app/components/NewsItems/NewsItem.js
+++ app/components/NewsItems/NewsItem.js
@@ -74,7 +74,7 @@ const NewComment = ({ newsItemId }) => {
 }
 
 const Comment = ({ comment }) => {
-  const { isLoading, isError, data: user, error } = useQuery(['users', comment.user_id], getUser)
+  const commentUser = comment.user
   const { t } = useTranslation('generic')
   const queryCache = useQueryCache()
   const onClickRemove = () => {
@@ -85,9 +85,8 @@ const Comment = ({ comment }) => {
   return (
     <Row style={{ borderTop: '1px solid #eee', paddingTop: 10, display: 'flex', alignItems: 'center' }}>
       <Col md={2} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        {isLoading && <DefaultSpinner />}
-        {user && <Image src={user.avatar_url_thumb} roundedCircle />}
-        {user && <h5>{user.name}</h5>}
+        {commentUser && <Image src={commentUser.avatar_url_thumb} roundedCircle />}
+        {commentUser && <h5>{commentUser.name}</h5>}
       </Col>
       <Col md={8} style={{ display: 'flex', alignItems: 'center' }}>
         {comment.commenttext}
